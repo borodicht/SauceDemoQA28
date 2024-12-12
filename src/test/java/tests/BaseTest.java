6dbf4591cdfd5eb1c9994d99fa414b0c4e2afe71
Borodich Timofei <borodich.timofei@gmail.com>
2024-12-12 09:25:34 +0300

    TMS-3 added PropertyReader

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -35,8 +35,8 @@ public class BaseTest {
     public void setup(@Optional("chrome") String browser, ITestContext context) {
         if (browser.equalsIgnoreCase("chrome")) {
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--no-sandbox");
-//            options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
 //            options.addArguments("--disable-gpu");
 //            options.addArguments("--disable-extensions");
 //            options.addArguments("--remote-allow-origins=*");
