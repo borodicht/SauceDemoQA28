23afeed1a9c83c7eca9a3e583b65d323cfcc52f6
Borodich Timofei <borodich.timofei@gmail.com>
2025-01-09 22:38:28 +0300

    added page

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -35,11 +35,11 @@ public class BaseTest {
     public void setup(@Optional("chrome") String browser, ITestContext context) {
         if (browser.equalsIgnoreCase("chrome")) {
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--remote-allow-origins=*");
+//            options.addArguments("--no-sandbox");
+//            options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--disable-gpu");
+//            options.addArguments("--disable-extensions");
+//            options.addArguments("--remote-allow-origins=*");
 //            if (System.getProperty("headless", "true").equals("true")) {
 //                options.addArguments("--headless");
 //            }
